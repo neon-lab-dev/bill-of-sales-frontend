3ae5b6c538a745f15d32fa6ea7839da1a0587761
jrtilak <iamjrtilak@gmail.com>
2024-06-27 20:06:23 +0545

    refactor: Remove unused code and update template page layout

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import TemplateCard from "@/components/TemplateCard";
 
 const SearchPage = () => {
@@ -9,13 +7,7 @@ const SearchPage = () => {
         <h2 className="text-gray-500 text-[34px] md:text-[42px] font-500 leading-[120%]">
           Alabama Bill of Sale Forms
         </h2>
-        <p className="text-black/65 text-sm md:text-base">
-          Alabama bill of sale forms are used as the written contract for any
-          vehicle, vessel, or thing that can be purchased and sold. The most
-          common type is for automobiles and this type of form must be completed
-          and submitted for registration within twenty (20) days to any
-          Department of Revenue location by County (Find Office).
-        </p>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <TemplateCard />
           <TemplateCard />
