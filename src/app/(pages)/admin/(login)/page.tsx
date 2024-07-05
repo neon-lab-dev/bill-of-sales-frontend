1f4b3708c2a3f6a91c7d8a39344d0fd1c0bc6f57
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-07-05 17:07:51 +0530

    fix:deployment error

--- src/app/(pages)/admin/(login)/page.tsx
+++ src/app/(pages)/admin/(login)/page.tsx
@@ -1,6 +1,7 @@
 import { ICONS, IMAGES } from '@/assets'
 import Button from '@/components/Button'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Login = () => {
@@ -9,19 +10,19 @@ const Login = () => {
             <div className=' flex flex-col w-1/2 p-[140px] bg-white'>
                 <Image src={ICONS.login} alt='logo' className='py-16 ml-28' />
                 <div class="relative mb-6 ml-16">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                     <Image src={IMAGES.mail} alt='lock' className=''/>
                     </div>
-                    <input type="text" id="input-group-1" class=" text-lg rounded-lg block border w-[400px] ps-10 p-2.5 placeholder:text-[#333333] " placeholder="name@flowbite.com" />
+                    <input type="text" id="input-group-1" class=" bg-white text-lg rounded-lg block border w-[400px] ps-10 p-2.5 placeholder:text-[#333333] " placeholder="name@flowbite.com" />
                 </div>
                 <div class="relative mb-6 ml-16">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                         <Image src={IMAGES.lock} alt='lock'/>
                     </div>
-                    <input type="text" id="input-group-1" class=" text-lg rounded-lg block border w-[400px] ps-10 p-2.5 placeholder:text-[#333333] " placeholder="name@flowbite.com" />
+                    <input type="text" id="input-group-1" class=" bg-white text-lg rounded-lg block border w-[400px] ps-10 p-2.5 placeholder:text-[#333333] " placeholder="************" />
                 </div>
                 <div className=' flex justify-center text-center ml-8'>
-                    <button  className=' text-white bg-[#3E6FBF] p-4 rounded-xl ml-2 text-center w-[400px]'><span className=' text-center'>Login</span></button>
+                    <button  className=' text-white bg-[#3E6FBF] p-4 rounded-xl ml-2 text-center w-[400px]'><span className=' text-center'><Link href="/admin/dashboard">Login</Link></span></button>
                 </div>
             </div>
             <div className=' flex justify-center  bg-[#D8DEE8] w-1/2 h-screen '>
