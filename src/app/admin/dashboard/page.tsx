f7be66c606a2752e1af6e61e4c4d56cca33060ad
MekalaArunKumar16 <mekalaarunkumar2002@gmail.com>
2024-07-06 23:34:48 +0530

    fix:error

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -33,17 +33,6 @@ const Dashboard = () => {
               <hr />
               <div className="flex  mt-6">
                 <span className=" text-lg font-thin">Just updated</span>
-    <AdminNavbar/>
-    <div>
-      <div className='flex'>
-        <AdminSiderbar/>
-        <div className='p-6 w-[320px] h-[200px] shadow-xl m-10 rounded-xl bg-white'>
-          <div className='flex flex-col gap-2'>
-            <div className='flex justify-between '>
-              <Image src={ICONS.note} alt='' />
-              <div className='flex flex-col'>
-                <span className=' text-[#7B809A] mt-1'>No .of Bills</span>
-                <span  className='text-lg font-700 text-end'>250</span>
               </div>
             </div>
           </div>
